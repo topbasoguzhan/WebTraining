151c1f4bebc2cfd642be7bbbee58cbebb3891887
Oğuzhan Topbaş <topbasoguzhan@gmail.com>
2022-01-15 19:06:26 +0300

    TagHelpers

---
+++ SampleProject/Extensions/Extension1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SampleProject.Extensions
+{
+    static public class Extension1
+    {
+        public static IHtmlContent CustomTextBox(this IHtmlHelper htmlHelper, string name, string value, string placeHolder) =>
+            htmlHelper.TextBox(name, value, new
+            {
+                style = "background-color:green;color:white;font-size:11px",
+                @class = "form-input", //class özel bir keyword olduğu için isim olarak kullanabilmek için başına @ koyduk.
+                a = "a",
+                b = "b",
+                placeHolder = placeHolder
+            });
+    }
+}
